3b05e4c9205c602a9aefd38d2028711e846197d6
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-09-27 11:45:13 +0300

    Спрятал свитч выбора объекта в SlideObject

--- src/Modules/Slide/Slide.tsx
+++ src/Modules/Slide/Slide.tsx
@@ -1,6 +1,5 @@
 import { GradientColor, ImageSrc, ImageType, SolidColor, TextAreaType } from "../../Model/types"
-import { TextArea } from "../TextArea/TextArea"
-import { Image } from "../Image/Image"
+import { SlideObject } from "../SlideObject/SlideObject"
 import style from './Slide.module.css'
 
 type SlideProps = {
@@ -35,27 +34,11 @@ function Slide(slideProps: SlideProps) {
     return (
         <div style={slideStyle} className={style.slide}>
             {
-                slideProps.objects.map(object => {
-                    switch (object.type) {
-                        case 'textObj':
-                            return <TextArea
-                                key={object.id}
-                                value={object.value}
-                                font={object.font}
-                                color={object.color}
-                                textSize={object.textSize}
-                                pos={object.pos}
-                                size={object.size}
-                                turnAngle={object.turnAngle} />
-                        case 'imageObj':
-                            return <Image
-                                key={object.id}
-                                src={object.src}
-                                pos={object.pos}
-                                size={object.size}
-                                turnAngle={object.turnAngle} />
+                slideProps.objects.map(
+                    object => {
+                        return <SlideObject object={object} />
                     }
-                })
+                )
             }
         </div>
     )
---
+++ src/Modules/SlideObject/SlideObject.tsx
@@ -0,0 +1,40 @@
+import { ImageType, TextAreaType } from "../../Model/types"
+import { TextArea } from "../TextArea/TextArea"
+import { Image } from "../Image/Image"
+
+type SlideObjectProps = {
+    object: TextAreaType | ImageType
+}
+
+function SlideObject(slideObjectProps: SlideObjectProps) {
+    let object
+    switch (slideObjectProps.object.type) {
+        case 'textObj':
+            object = <TextArea
+                key={slideObjectProps.object.id}
+                value={slideObjectProps.object.value}
+                font={slideObjectProps.object.font}
+                color={slideObjectProps.object.color}
+                textSize={slideObjectProps.object.textSize}
+                pos={slideObjectProps.object.pos}
+                size={slideObjectProps.object.size}
+                turnAngle={slideObjectProps.object.turnAngle} />
+            break
+        case 'imageObj':
+            object = <Image
+                key={slideObjectProps.object.id}
+                src={slideObjectProps.object.src}
+                pos={slideObjectProps.object.pos}
+                size={slideObjectProps.object.size}
+                turnAngle={slideObjectProps.object.turnAngle} />
+            break
+    }
+
+    return (
+        <div>
+            {object}
+        </div>
+    )
+}
+
+export { SlideObject }
