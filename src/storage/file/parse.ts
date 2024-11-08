12f555adecd0f20da95177b0563be668ef5dddb1
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-11-08 12:36:53 +0300

    Убрал лишний код

--- src/storage/file/parse.ts
+++ src/storage/file/parse.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PresentationType, SelectionType } from "../types"
 import { validatePresentation, validateSelection } from "./validation/ajv"
 
